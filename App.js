d95c751f82f85cf20612a64e1165a3d9742a6ede
jibon969 <jayed.swe@gmail.com>
2024-03-29 05:22:48 +0600

    added How AsyncStorage Work readme file

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import {StyleSheet, Text, View} from 'react-native';
-import SetGetRemove from './src/Learn/AsyncStorage/SetGetRemove';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
+  const [user, setUser] = useState('');
+
+  const setData = async () => {
+    // Here is name is key and value is jibon
+    await AsyncStorage.setItem('name', 'jibon');
+  };
+
+  const getData = async () => {
+    // // when we get data we can call key property
+    const name = await AsyncStorage.getItem('name');
+    setUser(name);
+    // console.log(name);
+  };
+
+  const removeData = async () => {
+    // when we remove data we can also call key property
+    await AsyncStorage.removeItem('name');
+    setUser('');
+  };
+
   return (
     <View style={styles.container}>
-      <SetGetRemove />
+      <Text style={styles.heading}>AsyncStorage set, get, remove data</Text>
+      <Button title="Set data" onPress={setData} />
+      <Text style={styles.text}>Show Data: {user}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Get data" onPress={getData} />
+        <View style={styles.buttonGap} />
+        <Button title="Remove data" onPress={removeData} />
+      </View>
     </View>
   );
 };
@@ -21,5 +49,14 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+  },
+  text: {
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  buttonGap: {
+    width: 10,
   },
 });
